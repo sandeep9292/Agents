12647772d1d94603a37d5cb1959526c15e0b787f
Sandeep Srinivas <sandeepsrinivas@Sandeeps-MacBook-Pro.local>
2025-05-06 10:58:55 -0700

    my first agent POC

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/anthropics/anthropic-sdk-go"
+	"github.com/anthropics/anthropic-sdk-go/option"
+)
+
+func main() {
+	client := anthropic.NewClient(option.WithAPIKey("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"))
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	getUserMessage := func() (string, bool) {
+		if !scanner.Scan() {
+			return "", false
+		}
+		return scanner.Text(), true
+	}
+	agent := NewAgent(&client, getUserMessage)
+	err := agent.Run(context.TODO())
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	}
+
+}
+
+func NewAgent(client *anthropic.Client, getUserMessage func() (string, bool)) *Agent {
+	return &Agent{
+		client:         client,
+		getUserMessage: getUserMessage,
+	}
+}
+
+type Agent struct {
+	client         *anthropic.Client
+	getUserMessage func() (string, bool)
+}
+
+func (a *Agent) Run(ctx context.Context) error {
+	conversation := []anthropic.MessageParam{}
+	fmt.Println("chat with Claude (user 'ctrl-c' to quit)")
+	for {
+		fmt.Print("\u001b[94mYou\u001b[0m: ")
+		userInput, ok := a.getUserMessage()
+		if !ok {
+			break
+		}
+
+		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+		conversation = append(conversation, userMessage)
+
+		message, err := a.runInference(ctx, conversation)
+		if err != nil {
+			return err
+		}
+		conversation = append(conversation, message.ToParam())
+
+		for _, content := range message.Content {
+			switch content.Type {
+			case "text":
+				fmt.Printf("\u001b[93mClaude\u001b[0m: %s\n", &content.Text)
+
+			}
+		}
+
+	}
+	return nil
+}
+
+func (a *Agent) runInference(ctx context.Context, conversation []anthropic.MessageParam) (*anthropic.Message, error) {
+	message, err := a.client.Messages.New(ctx, anthropic.MessageNewParams{
+		Model:     anthropic.ModelClaude3_7SonnetLatest,
+		MaxTokens: int64(1024),
+		Messages:  conversation,
+	})
+	return message, err
+}
